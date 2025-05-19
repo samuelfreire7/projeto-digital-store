0c1be517358327769d5bfac1a3364871219024fa
samuelfreire7 <samuelfgwm@gmail.com>
2025-05-19 16:41:24 -0300

    Tentativa Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Footer from './components/Footer'
+import ProductCard from './components/ProductCard'
 
 function App() {
   return (
     <>
+      <ProductCard />
       <Footer />
     </>
   )
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,23 @@
+import "./ProductCard.css"
+
+export default function ProductCard() {
+    return (
+        <>
+            <div class="colecoes">
+
+                <div className="card">
+                    <a href="#" /><img src="images/Teniskswiss.png" className="card-img-top" alt="..." />
+                    <div className="card-body body_em_alta" >
+                        <p className="card-title">Tênis</p>
+                        <p className="card-text texto_em_alta">K-Swiss V8 - Masculino</p>
+                        <div className="presos">
+                            <p className="valor_em_alta">R$ 200</p>
+                            <p className="descontos">R$ 100</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
