d3ccf1349cf1dce84600b4504b880419ea28edc4
samuelfreire7 <samuelfgwm@gmail.com>
2025-05-23 15:42:47 -0300

    Refatorando coleção parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Footer from './components/Footer'
 import ProductCard from './components/ProductCard'
 import Section from './components/Section'
 import sapatoAzul from './assets/images/sapato_azul_Dstore.png';
+import colecaoBlusa from './assets/images/blusaSupreme.png';
+import colecaoFone from './assets/images/cardFone.png';
+import colecaoTenis from './assets/images/cardTenis.png';
+
 
 
 function App() {
@@ -56,6 +60,20 @@ function App() {
       desconto: "R$ 100"
     }
   ];
+  const colecoes = [
+    {
+      imagem: colecaoBlusa,
+      titulo: "Novo drop Supreme",
+    },
+    {
+      imagem: colecaoTenis,
+      titulo: "Coleção Adidas",
+    },
+    {
+      imagem: colecaoFone,
+      titulo: "Novo Beats Bass",
+    }
+  ];
   return (
     <>
 
@@ -71,7 +89,15 @@ function App() {
         ))}
       </div>
 
-      <Section />
+      <div>
+        {colecoes.map((colecao, index) => (
+          <Section
+            key={index}
+            titulo={colecao.titulo}
+            imagem={colecao.imagem}
+          />
+        ))}
+      </div>
       <Footer />
     </>
   )
