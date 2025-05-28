9a6bd823afb9beac33adaabdf13b8cd5327c5fbe
Pedro Juan <juanximendes00@gmail.com>
2025-05-28 15:11:26 -0300

    corrigindo o carrossel pagi3

--- src/components/Carrossel.jsx
+++ src/components/Carrossel.jsx
@@ -60,12 +60,7 @@ export default function Carrossel() {
           ))}
         </div>
 
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="prev"
-        >
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
         </button>
